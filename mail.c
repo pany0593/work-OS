b8d847aa2f9077aa169883a1ad2308ee23db4efd
pany0593 <1569987504@qq.com>
2024-11-16 22:22:08 +0800

    add: receive_mail function

--- mail.c
+++ mail.c
@@ -57,8 +57,44 @@ int send_mail(pid_t pid, struct Mail* mail)
 //接收成功返回1,失败返回-1
 int receive_mail()
 {
+    key_t shmkey = get_shmkey(getpid()); // utils
+    int shmid = get_shmid(shmkey, (size_t)sizeof(struct MailBox)); // utils
+    if(shmid == -1)
+    {
+        printf("邮箱不存在");
+        return -1; // 如果共享内存ID无效，说明邮箱不存在
+    }
+    // 获取当前进程的邮箱地址（共享内存中的邮箱结构体）
+    struct MailBox* mailbox = (struct MailBox*)get_mailbox(shmid); // utils
 
+    // 如果邮箱为空，没有邮件可以接收
+    if(mailbox->mail_num == 0)
+    {
+        printf("邮箱为空，没有邮件可以接收\n");
+        return -1;
+    }
+
+    // 获取当前接收到的邮件，按接收索引读取邮件
+    struct Mail *mail = malloc(sizeof(struct Mail));
+    memcpy(mail, &mailbox->mails[mailbox->rec_index], sizeof(struct Mail));
+
+    printf("message:%s\n",mail->message);
+
+    mailbox->rec_index++;
+
+    if(mailbox->rec_index == MAIL_MAX_NUM)
+        mailbox->rec_index = 0;
+
+    // 减少邮箱中的邮件数量
+    mailbox->mail_num--;
+
+    if(mailbox->mail_num == 0)
+        mailbox->rec_index = -1;
+
+    printf("接收邮件成功\n");
+    return 1;
 }
+
 
 //撤销最近发送的一封邮件
 //撤销成功返回1,失败返回-1
--- test.c
+++ test.c
@@ -1,23 +1,49 @@
 #include "mail.h"
 
-int main()
+void send()
 {
-    printf("PID:%d\n",(int)getpid());
-    create_mailbox();
-
     int rec_pid;
     char message[1000];
     printf("发送邮件给：");
     scanf("%d",&rec_pid);
     printf("邮件内容：");
     scanf("%s",message);
-
     struct Mail *mail = malloc(sizeof(struct Mail));
     mail->sender_pid = getpid();
     memcpy(mail->message,message,sizeof(message));
-
     send_mail(rec_pid, mail);
+}
+
+void quash()
+{
+    int rec_pid;
+    printf("撤销的邮箱：");
+    scanf("%d",&rec_pid);
     quash_mail(rec_pid);
+}
+
+int main()
+{
+    printf("PID:%d\n",(int)getpid());
+    create_mailbox();
+
+    char opt[10];
+    while(1)
+    {
+        printf("输入操作：");
+        scanf("%s",opt);
+        if(strcmp(opt,"exit")==0)
+        {
+            delete_mailbox();
+            return 0;
+        }
+        else if(strcmp(opt,"send")==0)
+            send();
+        else if(strcmp(opt,"receive")==0)
+            receive_mail();
+        else if(strcmp(opt,"quash")==0)
+            quash();
+    }
     delete_mailbox();
     return 0;
 }
